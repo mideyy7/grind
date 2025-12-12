79c7df9ccc6115d78eed97be805b21a667be1c0e
Ayomide <mideyy7@gmail.com>
2025-12-12 17:23:54 +0000

    V2 - Styling the plot

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ import numpy as np
 x = np.array([1,2,3,4,5,6,7,8,9])
 y = np.array([10,20,30,40,50,60,70,80, 90])
 
-plt.plot(x,y)
+style = dict(marker = ".", ms = 10, markerfacecolor = "red", markeredgecolor = "red", linestyle = "dotted", linewidth = 2, color = "purple")
+plt.show()
+
+
+print(style)
+plt.plot(x,y, **style)
 plt.show()
